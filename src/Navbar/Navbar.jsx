1dd23f27b6966e88fa1bfe6747fdee3f66618668
Peace Melodi <peacemelodi1@gmail.com>
2024-05-12 21:26:49 +0100

    new

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,30 +1,61 @@
 import React, { useState }  from 'react'
 import './Navbar.css'
 import Logo from './Bytechain-11.png'
+import { RiMenu2Line } from "react-icons/ri";
+import { IoMdClose } from "react-icons/io";
+import Sign from './Bytechain-19.png'
+
 
 const Navbar = () => {
-
-  const [isOpen, setIsOpen] = useState(false);
-  const closeNavbar = () => {
-    setIsOpen(false);
+  const [activeButton, setActiveButton] = useState('Home'); // Initialize active button state with 'Home'
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  // Function to handle click on a button
+  const handleButtonClick = (buttonName) => {
+    setActiveButton(buttonName === activeButton ? 'Home' : buttonName);
   };
+   
 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
 
   return (
     <div className='Navbar'>
       <div className="navbar-wall">
-      <img src={Logo} alt="" />
+      <div className="bar-icon"  onClick={toggleMenu}><RiMenu2Line /></div>
+      <img src={Logo} alt="logo"/>
       <div className="nav-list">
-        <a href="">Home</a>
-        <a href="">About Us</a>
-        <a href="">Services</a>
-        <a href="">Projects</a>
-        <a href="">Careers</a>
-        <a href="">Blog</a>
-        <a href="">Contact</a>
-      </div>
+        <a href="#" className={activeButton === 'Home' ? 'active' : ''}
+            onClick={() => handleButtonClick('Home')}>Home</a>
+        <a href="#" className={activeButton === 'About Us' ? 'active' : ''}
+            onClick={() => handleButtonClick('About Us')}>About Us</a>
+        <a href="#" className={activeButton === 'Services' ? 'active' : ''}
+            onClick={() => handleButtonClick('Services')}
+      >Services</a>
+        <a href="#"  className={activeButton === 'Projects' ? 'active' : ''}
+            onClick={() => handleButtonClick('Projects')}>Projects</a>
+        <a href="#"  className={activeButton === 'Careers' ? 'active' : ''}
+            onClick={() => handleButtonClick('Careers')}
+       >Careers</a>
+        <a href="#" className={activeButton === 'Blog' ? 'active' : ''}
+            onClick={() => handleButtonClick('Blog')}>Blog</a>
+        <a href="#" className={activeButton === 'Contact' ? 'active' : ''}
+            onClick={() => handleButtonClick('Contact')}>Contact</a>
+        </div>
+        <div className={`nav-list2 ${isMenuOpen ? 'open' : ''}`}>
+          <div className="iconx">
+          <IoMdClose onClick={toggleMenu}/>
+          </div>          <div className="sign">
+          <img src={Sign} alt="" className=''/>
+          </div>
+          <a href="#">Home</a>
+          <a href="#">About</a>
+          <a href="#">Project</a>
+          <a href="#">Blog</a>
+          <a href="#">Contact</a>
 
+        </div>
       </div>
          </div>
     )
