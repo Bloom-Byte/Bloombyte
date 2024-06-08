7e7f5e7b5d6eeecfa513ac1c50f30363c21c4aed
Peace Melodi <peacemelodi1@gmail.com>
2024-06-08 15:30:34 +0100

    new

--- src/main-pages/Projects/ProjectThree/ProjectThree.jsx
+++ src/main-pages/Projects/ProjectThree/ProjectThree.jsx
@@ -3,13 +3,59 @@ import { useNavigate } from 'react-router-dom';
 import './ProjectThree.css'
 import { HiMiniArrowSmallLeft } from "react-icons/hi2";
 import { MdArrowOutward } from "react-icons/md";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 const ProjectThree = () => {
 
   const navigate = useNavigate();
 
   const handleBackClick = () => {
-    navigate('/projects'); 
+    navigate(-1); 
   };
+
+ 
+
+    var settings = {
+      infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        initialSlide: 0,
+      arrows: true,
+      autoplay: false, 
+      autoplaySpeed: 2000, 
+
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+            
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+                initialSlide: 2,
+                infinite: true,
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 2,
+                slidesToScroll: 2,
+                infinite: true,
+            }
+          }
+        ]
+      };
 
   return (
       <section className='projectree-wall'>
@@ -94,8 +140,53 @@ const ProjectThree = () => {
           <p>Bytechain followed an Agile methodology, with regular sprint planning, daily stand-ups, and retrospective meetings to ensure continuous collaboration and iterative development.</p>
     </div>
       </div>
+      <div className="boxy-heading"><h3>Project Images</h3></div>
+      <div className="projectree-box">
+
+      <Slider {...settings} className='slider-x'>
+        <div className="projectree-boxy1">
+
+        </div>
+        <div className="projectree-boxy1">
+          
+        </div>
+        
+        <div className="projectree-boxy1">
+          
+        </div>
+        <div className="projectree-boxy1">
+          
+        </div>
+        <div className="projectree-boxy1">
+          
+          </div>
+          </Slider>
+       </div>
+       <div className="boxy-heading"><h3>Project Videos</h3></div>
+      <div className="projectree-box">
+
+      <Slider {...settings} className='slider-x'>
+        <div className="projectree-boxy1">
+
+        </div>
+        <div className="projectree-boxy1">
+          
+        </div>
+        
+        <div className="projectree-boxy1">
+          
+        </div>
+        <div className="projectree-boxy1">
+          
+        </div>
+        <div className="projectree-boxy1">
+          
+          </div>
+          </Slider>
+       </div>
     </section>
-  )
+
+    )
 }
 
 export default ProjectThree
