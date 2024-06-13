d3b88deae01b5234fdf53262ec8cf4710d1f6585
Peace Melodi <peacemelodi1@gmail.com>
2024-06-13 20:02:53 +0100

    new

--- src/main-pages/Careers/CareersTwo/CareersTwo.jsx
+++ src/main-pages/Careers/CareersTwo/CareersTwo.jsx
@@ -1,19 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './CareersTwo.css'
 import { IoSearchOutline } from "react-icons/io5";
+import { MdArrowOutward } from "react-icons/md";
+import jobPositions from './jobPositions';
+import IMAGECARD from './card.png'
 
 const CareersTwo = () => {
+
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filteredJobs, setFilteredJobs] = useState(jobPositions);
+
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSearchClick = () => {
+    const formattedSearchTerm = searchTerm.toLowerCase().replace(/[\s\-\/]/g, '');
+    const filtered = jobPositions.filter(job =>
+      job.title.toLowerCase().replace(/[\s\-\/]/g, '').includes(formattedSearchTerm) ||
+      job.type.toLowerCase().replace(/[\s\-\/]/g, '').includes(formattedSearchTerm) ||
+      job.location.toLowerCase().replace(/[\s\-\/]/g, '').includes(formattedSearchTerm) ||
+      job.salary.toLowerCase().replace(/[\s\-\/]/g, '').includes(formattedSearchTerm)
+    );
+    setFilteredJobs(filtered);
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      handleSearchClick();
+    }
+  };
+
+
   return (
       <div className='careertwo'>
           <div className="careertwo-heading"><h1>Open Positions</h1></div>
           <div class="search-container">
               <div className="input-wrapper">
               <IoSearchOutline className='search-icon'/>
-        <input type="text" placeholder="Search Positions" className="search-input" />
+        <input type="text" placeholder="Search Positions" className="search-input"  value={searchTerm}
+            onChange={handleSearch}   onKeyDown={handleKeyDown} />
             
               </div>
-              <button>Search</button>
-    </div>
+              <button onClick={handleSearchClick}>Search</button>
+      </div>
+      <div className="careertwo-areabox">
+      {filteredJobs.map((job, index) => (
+        <div className="careertwo-areabox1">
+            <img src={IMAGECARD} alt=""  className='imagecard'/>
+      
+          <div className="careertwo-btns">
+            <button>{job.type}</button>
+            <button>{job.location}</button>
+            <button>{job.salary}</button>
+          </div>
+          <div className="xspecial">
+            <h3>{job.title}</h3>
+            <p>{job.description}</p>
+          </div>
+          <div className="apply">
+          <button>Apply <MdArrowOutward className='arrow-icon'/> </button>
+          </div>
+        </div>
+        ))}
+      
+      </div>
       </div> 
 )
 }
---
+++ src/main-pages/Careers/CareersTwo/jobPositions.js
@@ -0,0 +1,132 @@
+
+const jobPositions = [
+    {
+      type: 'Contract',
+      location: 'Remote',
+      salary: '₦200,000',
+      title: 'Senior Web Developer',
+      description: 'We are seeking an experienced Senior Web Developer to join our dynamic team.'
+    },
+    {
+      type: 'Full-time',
+      location: 'On-site',
+      salary: '₦300,000',
+      title: 'Junior Web Developer',
+      description: 'We are seeking an enthusiastic Junior Web Developer to join our growing team.'
+    },
+    {
+      type: 'Part-time',
+      location: 'Remote',
+      salary: '₦150,000',
+      title: 'Frontend Developer',
+      description: 'We are seeking an experienced Senior Web Developer to join our dynamic team.'
+    },
+    {
+      type: 'Full-time',
+      location: 'On-site',
+      salary: '₦400,000',
+      title: 'Backend Developer',
+      description: 'We are seeking an experienced Senior Web Developer to join our dynamic team.'
+    },
+    {
+      type: 'Contract',
+      location: 'Remote',
+      salary: '₦250,000',
+      title: 'Fullstack Developer',
+      description: 'We are seeking an experienced Senior Web Developer to join our dynamic team.'
+    },
+    {
+      type: 'Full-time',
+      location: 'Remote',
+      salary: '₦350,000',
+      title: 'DevOps Engineer',
+      description: 'We are seeking an experienced Senior Web Developer to join our dynamic team.'
+    },
+    {
+      type: 'Full-time',
+      location: 'Remote',
+      salary: '₦350,000',
+      title: 'DevOps Engineer',
+      description: 'We are seeking an experienced Senior Web Developer to join our dynamic team.'
+    },
+    {
+      type: 'Full-time',
+      location: 'On-site',
+      salary: '₦250,000',
+      title: 'Software Engineer',
+      description: 'We are seeking an experienced Senior Web Developer to join our dynamic team.'
+    },
+    {
+      type: 'Contract',
+      location: 'Remote',
+      salary: '₦200,000',
+      title: 'QA Tester',
+      description: 'We are seeking an experienced Senior Web Developer to join our dynamic team.'
+    },
+    {
+      type: 'Full-time',
+      location: 'On-site',
+      salary: '₦300,000',
+      title: 'Project Manager',
+      description: 'We are seeking an enthusiastic Junior Web Developer to join our growing team.'
+    },
+    {
+      type: 'Part-time',
+      location: 'Remote',
+      salary: '₦150,000',
+      title: 'UI/UX Designer',
+      description: 'We are seeking an experienced Senior Web Developer to join our dynamic team.'
+    },
+    {
+      type: 'Full-time',
+      location: 'On-site',
+      salary: '₦400,000',
+      title: 'Database Administrator',
+      description: 'We are seeking an experienced Senior Web Developer to join our dynamic team.'
+    },
+    {
+      type: 'Contract',
+      location: 'Remote',
+      salary: '₦250,000',
+      title: 'Product Manager',
+      description: 'We are seeking an experienced Senior Web Developer to join our dynamic team.'
+    },
+    {
+      type: 'Internship',
+      location: 'Remote',
+      salary: '₦100,000',
+      title: 'Marketing Intern',
+      description: 'We are seeking an enthusiastic Junior Web Developer to join our growing team.'
+    },
+    {
+      type: 'Full-time',
+      location: 'Remote',
+      salary: '₦350,000',
+      title: 'Graphic Designer',
+      description: 'We are seeking an experienced Senior Web Developer to join our dynamic team.'
+    },
+    {
+      type: 'Full-time',
+      location: 'On-site',
+      salary: '₦250,000',
+      title: 'Business Analyst',
+      description: 'We are seeking an experienced Senior Web Developer to join our dynamic team.'
+    },
+    {
+      type: 'Contract',
+      location: 'Remote',
+      salary: '₦200,000',
+      title: 'Content Writer',
+      description: 'We are seeking an experienced Senior Web Developer to join our dynamic team.'
+    },
+    {
+      type: 'Full-time',
+      location: 'On-site',
+      salary: '₦300,000',
+      title: 'System Administrator',
+      description: 'We are seeking an enthusiastic Junior Web Developer to join our growing team.'
+    }
+
+];
+  
+export default jobPositions;
