7956c70fc3051f4bfb4b67776cabb052800d7d6f
Peace Melodi <peacemelodi1@gmail.com>
2024-06-22 07:07:33 +0100

    new

--- src/main-pages/Blog/BlogThree/New.jsx
+++ src/main-pages/Blog/BlogThree/New.jsx
@@ -6,24 +6,24 @@ import './New.css'
 const New = () => {
   return (
     <section className='blogtwo2'>
-    <div className="blogtwo-heading2">
-        <h1>More Blog Posts</h1>
-    </div>
-    <div className="blogtwo-box2">
-        {newsData.map((news) => (<div key={news.id} className="blogtwo-boxone2">
-          <img src={IMAGECARD} alt=""  className='imagecard56'/>
-
-            <div className="white-phonex2"></div>
-            <p>{news.content}</p>
-            <span>{news.date}</span>
-            <div className="read-btn2">
-            <button>Read <MdArrowOutward/></button>    
-            </div>
-            
-        </div>
-          ))}
-    </div>
-</section>
+          <div className="blogtwo-heading2">
+              <h1>More Blog Posts</h1>
+          </div>
+          <div className="blogtwo-box2">
+              {newsData.map((news) => (<div key={news.id} className="blogtwo-boxone2">
+                <img src={IMAGECARD} alt=""  className='imagecardnew'/>
+      
+                  <div className="white-phonex2"></div>
+                  <p>{news.content}</p>
+                  <span>{news.date}</span>
+                  <div className="read-btn2">
+                  <button>Read <MdArrowOutward/></button>    
+                  </div>
+                  
+              </div>
+                ))}
+          </div>
+   </section>
   )
 }
 
